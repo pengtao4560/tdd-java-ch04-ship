61b3474e0bcceee5f3f889726bc6f8e2f25916a4
Viktor Farcic <viktor@farcic.com>
2015-04-06 00:00:57 +0200

    Finished

--- src/main/java/com/packtpublishing/tddjava/ch04ship/Ship.java
+++ src/main/java/com/packtpublishing/tddjava/ch04ship/Ship.java
@@ -2,4 +2,13 @@ package com.packtpublishing.tddjava.ch04ship;
 
 public class Ship {
 
+    private final Location location;
+    public Location getLocation() {
+        return location;
+    }
+
+    public Ship(Location location) {
+        this.location = location;
+    }
+
 }
--- src/test/java/com/packtpublishing/tddjava/ch04ship/ShipSpec.java
+++ src/test/java/com/packtpublishing/tddjava/ch04ship/ShipSpec.java
@@ -10,4 +10,10 @@ import static org.testng.Assert.*;
 @Test
 public class ShipSpec {
 
+    public void whenInstantiatedThenLocationIsSet() {
+        Location location = new Location(new Point(21, 13), Direction.NORTH);
+        Ship ship = new Ship(location);
+        assertEquals(ship.getLocation(), location);
+    }
+
 }
